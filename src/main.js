cf81bd3b3682f1fb20954855d661c393b431ddf9
Gleb Limanskiy <33000609+UnicalN@users.noreply.github.com>
2023-06-24 12:29:07 +0300

    fIX

--- src/main.js
+++ src/main.js
@@ -4,13 +4,14 @@ import PointsModel from './model/points-model.js';
 import FilterModel from './model/filter-model.js';
 import PointsApiService from './api-service.js';
 
-const AUTHORIZATION = 'Basic uniqueandORIGINALAUTHKEY';
+const AUTHORIZATION = 'Basic shmasikORIGINALA';
 const END_POINT = 'https://20.ecmascript.pages.academy/big-trip';
 
 const filterContainer = document.querySelector('.trip-controls__filters');
 const pointListContainer = document.querySelector('.trip-events');
-
-const pointsModel = new PointsModel({pointsApiService: new PointsApiService(END_POINT, AUTHORIZATION)});
+const pointsApiService = new PointsApiService(END_POINT, AUTHORIZATION);
+console.log(pointsApiService.points);
+const pointsModel = new PointsModel({pointsApiService});
 const filterModel = new FilterModel();
 
 pointsModel.init();
